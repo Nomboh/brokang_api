5fda2506480227471ff8f384b542f6295d93b5f0
Nomboh Quentin <nombohq14@gmail.com>
2022-09-10 19:14:06 +0900

    remove serve

--- index.js
+++ index.js
@@ -61,9 +61,8 @@ app.use((err, req, res, next) => {
     stack: err.stack,
   });
 });
-const PORT = process.env.PORT || 8800;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT || 8800, () => {
   // on connected
   connect();
   console.log(`Server is running on port ${PORT}`);
