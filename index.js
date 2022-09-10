945d093c7dd7653244bd502c92ad3c7905be3756
Nomboh Quentin <nombohq14@gmail.com>
2022-09-11 07:27:56 +0900

    change connectiong

--- index.js
+++ index.js
@@ -65,21 +65,6 @@ const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-process.on("unhandledRejection", err => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("SIGTERM", () => {
-  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
-  server.close(() => {
-    console.log("💥 Process terminated!");
-  });
-});
-
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3001",
